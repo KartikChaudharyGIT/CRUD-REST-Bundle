8ef5aea63fc3e2b087e8bf22928f9f5d669ceddd
KRTK <kartikchaudhary89@gmail.com>
2023-06-06 02:46:17 +0530

    Initial commit

---
+++ src/main/java/com/example/backend/CRUD/REST/Bundle/CrudRestBundleApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend.CRUD.REST.Bundle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestBundleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestBundleApplication.class, args);
+	}
+
+}
